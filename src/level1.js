25d6a1b06c5dd7747f5e410b3bbd5cf13db9cc4d
David Konsumer <konsumer@jetboystudio.com>
2019-07-31 23:28:44 -0700

    fix image paths

--- src/level1.js
+++ src/level1.js
@@ -5,17 +5,17 @@ import app from './app'
 const skySpeed = 0.1
 const citySpeed = 0.5
 
-const background = TilingSprite.from('Background-Night.png', 800, 800)
+const background = TilingSprite.from('./Background-Night.png', 800, 800)
 background.anchor.set(0)
 background.position.set(0)
 app.stage.addChild(background)
 
-const moon = Sprite.from('Moon-Night.png')
+const moon = Sprite.from('./Moon-Night.png')
 moon.setTransform(500, 100, 0.5, 0.5)
 moon.anchor.set(0.5)
 app.stage.addChild(moon)
 
-const city = TilingSprite.from('City-Block.png', 800, 800)
+const city = TilingSprite.from('./City-Block.png', 800, 800)
 city.anchor.set(0)
 city.setTransform(0, 200)
 app.stage.addChild(city)
